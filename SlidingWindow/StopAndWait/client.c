622aa0ae9d0d8dd045b180a5b37926b72d954add
Adithya Sailesh <97180744+Adithya-Sailesh@users.noreply.github.com>
2024-07-10 11:09:24 +0530

    Update client.c

--- SlidingWindow/StopAndWait/client.c
+++ SlidingWindow/StopAndWait/client.c
@@ -1,5 +1,6 @@
-//first run server.c
-//gcc Server.c
+// First run server.c then ./a.out
+// Second run client.c them ./a.out
+// Give Port number has 4000(or any 4digitNumber)
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
